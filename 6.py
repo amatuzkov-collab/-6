39f5f4a8d913bfa22395e682eb5625a12dde565d
amatuzkov-collab <a.matuzkov@g.nsu.ru>
2025-12-16 11:14:29 +0700

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,6 +1,8 @@
-import math
-n,k,m=map(int,input().split())
-raz=(math.ceil(n/k))*2 #сколько прокрутов карусели, каждый по 2 раза
-time=raz*m
-print(time)
+import math 
 
+N, K, M = map(int, input().split())
+
+sessions = math.ceil(N/K)*2
+time = sessions * M
+
+print(time)
