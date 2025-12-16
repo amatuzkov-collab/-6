4b79b84c384aa2b758c80affc6009497440bde18
amatuzkov-collab <a.matuzkov@g.nsu.ru>
2025-12-16 11:16:36 +0700

    7.py

--- 7.py
+++ 7.py
@@ -1,9 +1,11 @@
 k=int(input())
+
 flag=False
 for x in range(0,100):
     for y in range(0,100):
         if x*5+y*7==k:
             flag=True
+            
 if flag==True:
     print('да')
 else:
